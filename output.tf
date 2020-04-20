71d8d517994028989769d0f5296559f7153eb1f5
dnyaneshwar-LT <dnyanesh1705@gmail.com>
2020-04-20 20:55:19 +0530

    corrected syntax

--- output.tf
+++ output.tf
@@ -3,17 +3,17 @@ output "alb_output" {
 }
 
 output "ecs_cluster_output" {
-  value = aws_ecs_cluster.demo.dns_name
+  value = aws_ecs_cluster.demo.name
 }
 
 output "ecs_service_output" {
-  value = aws_ecs_service.springboot.dns_name
+  value = aws_ecs_service.springboot.name
 }
 
 output "vpc_output" {
-  value = aws_vpc.demo-tf.dns_name
+  value = aws_vpc.demo-tf.name
 }
 
 output "traget_group" {
-    value = aws_alb_target_group.springboot.dns_name
+    value = aws_alb_target_group.springboot.name
 }
